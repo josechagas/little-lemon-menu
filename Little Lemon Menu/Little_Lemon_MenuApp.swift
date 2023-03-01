b4c8da0563ac48f9f861c2c856f5e1bf057fc153
José Lucas Souza das Chagas <chagasjoselucas@gmail.com>
2023-03-01 16:32:40 -0300

    Initial Commit

---
+++ Little Lemon Menu/Little_Lemon_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_MenuApp.swift
+//  Little Lemon Menu
+//
+//  Created by José Lucas Souza das Chagas on 01/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
